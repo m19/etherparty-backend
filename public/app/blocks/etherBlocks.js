bc942dde6b713408feb05dd1e19498cc110e98a6
Martijn Buurman <martijn@m19.nl>
2015-04-11 18:00:01 +0200

    Added extra blocks

--- public/app/blocks/etherBlocks.js
+++ public/app/blocks/etherBlocks.js
@@ -1,14 +1,23 @@
-var INFO_COLOR = 0,
-  OUTPUT_COLOR = 100,
-  MATH_COLOR = 150,
-  VALUE_COLOR = 200,
-  STATEMENT_COLOR = 250,
-  FLOW_COLOR = 300,
-  LOOP_COLOR = 350;
+var VALUE_COLOR = 190,
+  VAR_COLOR = 260,
+  FLOW_COLOR = 210,
+  LOOP_COLOR = 160,
+  MATH_COLOR = 230,
+  PROCEDURE_COLOR = 100,
+  STATEMENT_COLOR = 330,
+  COMMENT_COLOR = 58,
+  ARRAY_COLOR = 290;
+
+var valValidator = function (e) {
+  return e.replace(/[^a-z0-9_]/gi, '');
+};
+var varValidator = function (e) {
+  return e.replace(/^[0-9]+|[^a-z0-9_]/gi, '');
+};
 
 Blockly.Blocks['COMMENT'] = {
   init: function () {
-    this.setColour(INFO_COLOR);
+    this.setColour(COMMENT_COLOR);
     this.appendDummyInput()
       .appendField('note:')
       .appendField(new Blockly.FieldTextInput(''), 'COMMENT');
@@ -18,7 +27,7 @@ Blockly.Blocks['COMMENT'] = {
 
 Blockly.Blocks['VALUE'] = {
   init: function () {
-    this.setColour(OUTPUT_COLOR);
+    this.setColour(VALUE_COLOR);
     this.appendDummyInput()
       .appendField(new Blockly.FieldTextInput('0', Blockly.FieldTextInput.numberValidator), 'VALUE');
     this.setOutput(true);
@@ -27,7 +36,7 @@ Blockly.Blocks['VALUE'] = {
 
 Blockly.Blocks['TX'] = {
   init: function () {
-    this.setColour(OUTPUT_COLOR);
+    this.setColour(VALUE_COLOR);
     this.appendDummyInput()
       .appendField('tx')
       .appendField(new Blockly.FieldDropdown([['amount', 'AMOUNT'], ['origin', 'ORIGIN'], ['gas left', 'GAS LEFT'],
@@ -38,7 +47,7 @@ Blockly.Blocks['TX'] = {
 
 Blockly.Blocks['CONTRACT'] = {
   init: function () {
-    this.setColour(OUTPUT_COLOR);
+    this.setColour(VALUE_COLOR);
     this.appendDummyInput()
       .appendField('contract')
       .appendField(new Blockly.FieldDropdown([['caller', 'CALLER'], ['address', 'ADDRESS'], ['balance', 'BALANCE'],
@@ -49,7 +58,7 @@ Blockly.Blocks['CONTRACT'] = {
 
 Blockly.Blocks['BLOCKINFO'] = {
   init: function () {
-    this.setColour(OUTPUT_COLOR);
+    this.setColour(VALUE_COLOR);
     this.appendDummyInput()
       .appendField('block')
       .appendField(new Blockly.FieldDropdown([['timestamp', 'TIMESTAMP'], ['number', 'NUMBER'],
@@ -71,11 +80,11 @@ Blockly.Blocks['MATH'] = {
     ];
 
     this.setColour(MATH_COLOR);
-    this.setOutput(!0);
+    this.setOutput(true);
     this.appendValueInput('A').setCheck('Number');
     this.appendValueInput('B').setCheck('Number')
       .appendField(new Blockly.FieldDropdown(dropdown), 'OP');
-    this.setInputsInline(!0);
+    this.setInputsInline(true);
   }
 };
 
@@ -90,26 +99,26 @@ Blockly.Blocks['COMPARE'] = {
       ['>=', '>=']
     ];
     this.setColour(MATH_COLOR);
-    this.setOutput(!0);
+    this.setOutput(true);
     this.appendValueInput('A').setCheck('Number');
     this.appendValueInput('B').setCheck('Number')
       .appendField(new Blockly.FieldDropdown(dropdown), 'OP');
-    this.setInputsInline(!0);
+    this.setInputsInline(true);
   }
 };
 
-Blockly.Blocks['CONDITION'] = {
+Blockly.Blocks['LOGIC'] = {
   init: function () {
     var dropdown = [
       ['or', '||'],
       ['and', '&&']
     ];
     this.setColour(MATH_COLOR);
-    this.setOutput(!0);
+    this.setOutput(true);
     this.appendValueInput('A').setCheck('Number');
     this.appendValueInput('B').setCheck('Number')
       .appendField(new Blockly.FieldDropdown(dropdown), 'OP');
-    this.setInputsInline(!1);
+    this.setInputsInline(false);
   }
 };
 
@@ -119,13 +128,13 @@ Blockly.Blocks['LOAD'] = {
       ['save slot', 'sload'],
       ['temp slot', 'mload']
     ];
-    this.setColour(VALUE_COLOR);
+    this.setColour(VAR_COLOR);
     this.appendValueInput('SPOT')
       .appendField('data at')
       .appendField(new Blockly.FieldDropdown(dropdown), 'POOL')
       .setCheck('Number');
-    this.setOutput(!0);
-    this.setInputsInline(!1);
+    this.setOutput(true);
+    this.setInputsInline(false);
   }
 };
 
@@ -135,16 +144,16 @@ Blockly.Blocks['STORE'] = {
       ['save slot', 'sstore'],
       ['temp slot', 'mstore']
     ];
-    this.setColour(VALUE_COLOR);
+    this.setColour(VAR_COLOR);
     this.appendValueInput('SPOT')
       .appendField('in')
       .appendField(new Blockly.FieldDropdown(dropdown), 'POOL')
       .setCheck('Number');
     this.appendValueInput('VAL')
       .appendField('put');
-    this.setInputsInline(!1);
-    this.setPreviousStatement(!0);
-    this.setNextStatement(!0);
+    this.setInputsInline(false);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
   }
 };
 
@@ -155,9 +164,9 @@ Blockly.Blocks['SPEND'] = {
       .appendField('spend')
       .setCheck('Number');
     this.appendValueInput('TO').appendField('to');
-    this.setInputsInline(!0);
-    this.setPreviousStatement(!0);
-    this.setNextStatement(!0);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
   }
 };
 
@@ -165,7 +174,7 @@ Blockly.Blocks['STOP'] = {
   init: function () {
     this.setColour(STATEMENT_COLOR);
     this.appendDummyInput().appendField('stop');
-    this.setPreviousStatement(!0);
+    this.setPreviousStatement(true);
   }
 };
 
@@ -182,8 +191,8 @@ Blockly.Blocks['WHEN'] = {
       .appendField(new Blockly.FieldDropdown(dropdown), 'WORD');
     this.appendStatementInput('THEN')
       .appendField('then');
-    this.setPreviousStatement(!0);
-    this.setNextStatement(!0);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
   }
 };
 
@@ -195,8 +204,8 @@ Blockly.Blocks['IF'] = {
       .appendField('if');
     this.appendStatementInput('THEN').appendField('then');
     this.appendStatementInput('ELSE').appendField('else');
-    this.setPreviousStatement(!0);
-    this.setNextStatement(!0)
+    this.setPreviousStatement(true);
+    this.setNextStatement(true)
   }
 };
 
@@ -212,8 +221,8 @@ Blockly.Blocks['WHILE'] = {
       .appendField(new Blockly.FieldDropdown(e), 'WORD');
     this.appendStatementInput('DO')
       .appendField('repeat');
-    this.setPreviousStatement(!0);
-    this.setNextStatement(!0);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
   }
 };
 
@@ -223,8 +232,117 @@ Blockly.Blocks['INIT'] = {
     this.appendStatementInput('INIT')
       .appendField('init');
     this.appendStatementInput('BODY').appendField('body');
-    this.setInputsInline(!1);
-    this.setPreviousStatement(!1);
-    this.setNextStatement(!1);
+    this.setInputsInline(false);
+    this.setPreviousStatement(false);
+    this.setNextStatement(false);
+  }
+};
+
+Blockly.Blocks['CURRENCY'] = {
+  init: function () {
+    var currencies = [
+      ['wei', 'wei'],
+      ['Kwei', 'Kwei'],
+      ['Mwei', 'Mwei'],
+      ['Gwei', 'Gwei'],
+      ['szabo', 'szabo'],
+      ['finney', 'finney'],
+      ['ether', 'ether'],
+      ['Kether', 'Kether'],
+      ['Mether', 'Mether'],
+      ['Gether', 'Gether'],
+      ['Tether', 'Tether'],
+      ['Pether', 'Pether'],
+      ['Eether', 'Eether'],
+      ['Zether', 'Zether'],
+      ['Yether', 'Yether'],
+      ['Nether', 'Nether'],
+      ['Dether', 'Dether'],
+      ['Vether', 'Vether'],
+      ['Uether', 'Uether']
+    ];
+    this.setColour(VALUE_COLOR);
+    this.appendValueInput('AMT')
+      .setCheck('Number');
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(currencies), "DENOM");
+    this.setInputsInline(true);
+    this.setOutput(true);
+  }
+};
+
+Blockly.Blocks['INPUT'] = {
+  init: function () {
+    var ordinals = [
+      ['1st', '0'],
+      ['2nd', '1'],
+      ['3rd', '2'],
+      ['4th', '3'],
+      ['5th', '4'],
+      ['6th', '5'],
+      ['7th', '6'],
+      ['8th', '7'],
+      ['9th', '8'],
+      ['10th', '9'],
+      ['11th', '10'],
+      ['12th', '11'],
+      ['13th', '12'],
+      ['14th', '13'],
+      ['15th', '14'],
+      ['16th', '15'],
+      ['17th', '16'],
+      ['18th', '17'],
+      ['19th', '18'],
+      ['20th', '19']
+    ];
+    this.setColour(VALUE_COLOR);
+    this.setOutput(true);
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(ordinals), 'INDEX').appendField('input');
+    this.setInputsInline(true)
+  }
+};
+
+Blockly.Blocks['THINPUT'] = {
+  init: function () {
+    this.setColour(VALUE_COLOR);
+    this.setOutput(true);
+    this.appendValueInput('ORDINAL');
+    this.appendDummyInput()
+      .appendField("th input");
+    this.setInputsInline(true);
+  }
+};
+
+Blockly.Blocks['MSTORE'] = {
+  init: function () {
+    this.setColour(VAR_COLOR);
+    this.appendValueInput('VAL')
+      .appendField(new Blockly.FieldTextInput('', varValidator), 'SPOT')
+      .appendField('=');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+  }
+};
+
+Blockly.Blocks['MVAL'] = {
+  init: function () {
+    this.setColour(VAR_COLOR);
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldTextInput('', varValidator), 'SPOT');
+    this.setOutput(true);
+  }
+};
+
+Blockly.Blocks['RESERVE'] = {
+  init: function () {
+    this.setColour(VAR_COLOR);
+    this.appendValueInput('LEN')
+      .appendField('reserve');
+    this.appendDummyInput().appendField('temp slots');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
   }
 };
