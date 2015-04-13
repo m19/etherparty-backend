47364bd34b35135674c111df019f18150b3c19bb
Martijn Buurman <martijn@m19.nl>
2015-04-13 21:13:49 +0200

    Added more generators

--- public/app/components/blocky/generators/serpent.js
+++ public/app/components/blocky/generators/serpent.js
@@ -67,11 +67,12 @@ Blockly.Serpent.scrub_ = function (block, code) {
  * Order of operations
  */
 Blockly.Serpent.ORDER_ATOMIC = 0;
-Blockly.Serpent.ORDER_MULTIPLICATION = 5; // *
-Blockly.Serpent.ORDER_DIVISION = 5;       // /
-Blockly.Serpent.ORDER_MODULUS = 5;        // %
-Blockly.Serpent.ORDER_ADDITION = 6;       // +
-Blockly.Serpent.ORDER_SUBTRACTION = 6;    // -
+Blockly.Serpent.ORDER_MULTIPLICATION = 5;
+Blockly.Serpent.ORDER_DIVISION = 5;
+Blockly.Serpent.ORDER_MODULUS = 5;
+Blockly.Serpent.ORDER_ADDITION = 6;
+Blockly.Serpent.ORDER_SUBTRACTION = 6;
+Blockly.Serpent.ORDER_LOGICAL_NOT = 12;
 Blockly.Serpent.ORDER_NONE = 99;
 
 Blockly.Serpent.MAX_GAS = '(tx.gas - 100)';
@@ -216,4 +217,114 @@ Blockly.Serpent.MSTORE = function (block) {
   var val = Blockly.Serpent.valueToCode(block, 'VAL', order) || 0;
 
   return spot + ' = ' + val + '\n';
+};
+
+Blockly.Serpent.CURRENCY = function (block) {
+  var amount = Blockly.Serpent.valueToCode(block, 'AMT', Blockly.Serpent.ORDER_NONE) || 0;
+  var denominator = block.getFieldValue('DENOM');
+  var currencies = {
+    wei: "",
+    Kwei: "*10^3",
+    Mwei: "*10^6",
+    Gwei: "*10^9",
+    szabo: "*10^12",
+    finney: "*10^15",
+    ether: "*10^18",
+    Kether: "*10^21",
+    Mether: "*10^24",
+    Gether: "*10^27",
+    Tether: "*10^30",
+    Pether: "*10^33",
+    Eether: "*10^37",
+    Zether: "*10^40",
+    Yether: "*10^43",
+    Nether: "*10^45",
+    Dether: "*10^48",
+    Vether: "*10^51",
+    Uether: "*10^54"
+  };
+
+  var code = amount + currencies[denominator];
+
+  return [code, Blockly.Serpent.ORDER_ATOMIC];
+};
+
+Blockly.Serpent.MVAL = function (block) {
+  var value = block.getFieldValue('SPOT') || 0;
+  var code = '' + value;
+  return [code, Blockly.Serpent.ORDER_ATOMIC]
+};
+
+Blockly.Serpent.RESERVE = function (block) {
+  var value = Blockly.Serpent.valueToCode(block, 'LEN', Blockly.Serpent.ORDER_NONE) || 0;
+  return 'temp = array(' + value + '+1)\n';
+};
+
+Blockly.Serpent.LOAD = function (block) {
+  var pool = block.getFieldValue('POOL');
+  var spot = Blockly.Serpent.valueToCode(block, 'SPOT', Blockly.Serpent.ORDER_NONE) || '0';
+
+  var code = '';
+
+  if (pool == 'sload') {
+    code = 'contract.storage[' + spot + ']';
+  } else if (pool == 'mload') {
+    code = 'temp[' + spot + ']';
+  }
+
+  return [code, Blockly.Serpent.ORDER_ATOMIC];
+};
+
+Blockly.Serpent.STORE = function (block) {
+  var pool = block.getFieldValue('POOL');
+  var spot = Blockly.Serpent.valueToCode(block, 'SPOT', Blockly.Serpent.ORDER_NONE) || '0';
+  var value = Blockly.Serpent.valueToCode(block, 'VAL', Blockly.Serpent.ORDER_NONE) || 0;
+
+  var code = '';
+
+  if (pool == 'mstore') {
+    var i = 'temp[' + spot + ']';
+    code = i + ' = ' + value + '\n';
+  } else if (pool == 'sstore') {
+    code = 'contract.storage[' + spot + '] = ' + value + '\n';
+  }
+
+  return [code, Blockly.Serpent.ORDER_ATOMIC];
+};
+
+Blockly.Serpent.IF = function (block) {
+  var condition = Blockly.Serpent.valueToCode(block, 'COND', Blockly.Serpent.ORDER_NONE) || 1;
+  var thenCode = Blockly.Serpent.statementToCode(block, 'THEN');
+  var elseCode = Blockly.Serpent.statementToCode(block, 'ELSE');
+  return 'if ' + condition + ':\n' + thenCode + 'else:\n' + elseCode;
+};
+
+Blockly.Serpent.WHEN = function (block) {
+  var word = block.getFieldValue('WORD');
+  var condition = Blockly.Serpent.valueToCode(block, 'COND', Blockly.Serpent.ORDER_NONE) || 1;
+  var thenCode = Blockly.Serpent.statementToCode(block, 'THEN');
+
+  var code = '';
+  if (word == 'unless') {
+    code = 'if not (' + condition + ')';
+  } else {
+    code = 'if ' + condition;
+  }
+
+  return code + ':\n' + thenCode;
+};
+
+Blockly.Serpent.WHILE = function (block) {
+  var word = 'UNTIL' == block.getFieldValue('WORD');
+  var condition = Blockly.Serpent.valueToCode(block, 'COND', word ? Blockly.Serpent.ORDER_LOGICAL_NOT : Blockly.Serpent.ORDER_NONE) || 'false';
+  var doCode = Blockly.Serpent.statementToCode(block, 'DO');
+
+  var code = '';
+  if (word) {
+    code = 'while not (' + condition + ')';
+  } else {
+    code = 'while ' + condition;
+  }
+
+  return code + ':\n' + doCode;
 };
