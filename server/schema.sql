67e63d1261882a1bba337dd987ee6083fd78071d
chirag04 <jain_chirag04@yahoo.com>
2015-04-28 11:45:32 -0400

    update schema for stripe

--- server/schema.sql
+++ server/schema.sql
@@ -3,5 +3,7 @@ CREATE TABLE `user` (
   `username` varchar(100) NOT NULL DEFAULT '',
   `email` varchar(200) NOT NULL DEFAULT '',
   `password` varchar(64) NOT NULL DEFAULT '',
+  `plan_type` varchar(11) NOT NULL DEFAULT 'free',
+  `plan_expiry` date DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
