2f70135dfa47b07b89ed456063e1d75af9c85cdc
Martijn Buurman <martijn@m19.nl>
2015-04-28 19:52:02 +0200

    Added missing column to schema

--- server/schema.sql
+++ server/schema.sql
@@ -5,5 +5,8 @@ CREATE TABLE `user` (
   `password` varchar(64) NOT NULL DEFAULT '',
   `plan_type` varchar(11) NOT NULL DEFAULT 'free',
   `plan_expiry` date DEFAULT NULL,
+  `activation_token` varchar(32) DEFAULT NULL,
+  `activated` tinyint(1) DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
